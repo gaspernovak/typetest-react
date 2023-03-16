215ff171c62ad95a4a1be58c95f7f3d9f75abd33
Gašper Novak <gaspernovak@proton.me>
2023-03-16 20:07:08 +0100

    Added demo UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  function handleChange(event){
+    console.log(event.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <div className="Wordlist">
+      <p>
+        plague plain plan planet player plug podium poetry point police poodle pool pope popular porsche
+      </p>
+      <input className="input w-100" onChange={handleChange}/>
+      </div>
     </div>
   );
 }
