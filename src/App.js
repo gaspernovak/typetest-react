9f8dcb91df232849bc9a5d6d60e83e778095361f
Gašper Novak <gaspernovak@proton.me>
2023-03-16 22:05:39 +0100

    Added fetching wordlist from api
    input works

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
 import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios"
 
 function App() {
 
+  const [words, setWords] = useState([]);
+  const [word, setWord] = useState("")
+
   function handleChange(event){
-    console.log(event.target.value);
+    if(event.target.value == words[0] + " "){
+      let temp = words.slice(1);
+      setWords(temp) 
+      event.target.value = ''
+    }
+
+    console.log(event.target.value.lenght)
   }
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/wordlist").then((data) => {
+      setWords(data?.data);
+    });
+  }, []);
 
   return (
     <div className="App" >
       <div className="Wordlist">
-      <p>
-        plague plain plan planet player plug podium poetry point police poodle pool pope popular porsche
-      </p>
+        <p>
+          {words &&
+            words.map((word, key) =>
+              <span key={key}>{word} </span> 
+            )
+          }
+        </p>
       <input className="input w-100" onChange={handleChange}/>
       </div>
     </div>
