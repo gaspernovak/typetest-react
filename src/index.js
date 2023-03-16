c832320d38f0cfc68ee2f1299b8a419d14ccf3ee
Gašper Novak <gaspernovak@proton.me>
2023-03-17 00:29:51 +0100

    Updated imports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import Typetest from './Typetest';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -8,7 +8,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-    <App />
+    <Typetest />
   // {/* </React.StrictMode> */}
 );
 
