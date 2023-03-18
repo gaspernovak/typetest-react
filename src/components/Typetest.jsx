1930ca53e486122e2da9d7f303f9895ba0370461
Gašper Novak <gaspernovak@proton.me>
2023-03-18 01:28:25 +0100

    Moved detectError function out of component

--- src/components/Typetest.jsx
+++ src/components/Typetest.jsx
@@ -2,35 +2,44 @@ import './Typetest.css';
 import React, { useEffect, useState } from "react";
 import axios from "axios"
 
+function detectError(input, correctInput, wordElement) {
+  let match = correctInput.match(input)
+  if (match != input) {
+    wordElement.classList.add("Word-error")
+  } else {
+    wordElement.classList.remove("Word-error")
+  }
+}
+
+function calculateWpm(allEntries, timeSeconds) {
+  return Math.floor((allEntries / 5) / (timeSeconds / 60))
+}
+
 function Typetest() {
+  const timerValue = 30 
   const [words, setWords] = useState([]);
-  
   const [counter, setCounter] = useState(0);
 
-  function detectError(input, correctInput, wordElement){
-    let match = correctInput.match(input)
-    if (match != input) {
-      wordElement.classList.add("Word-error")
-    } else {
-      wordElement.classList.remove("Word-error")
-    }
-  }
+  const [wpm, setWpm] = useState(0);
+  const [entriesCounter, setEntriesCounter] = useState(0);
+
 
   function handleChange(event) {
-    let etv = event.target
-
-    if (counter == 0){
-      setCounter(30)
+    if (counter == 0) {
+      setCounter(timerValue)
     }
 
-    if (etv.value == words[0] + " ") {
-      let temp = words.slice(1);
-      setWords(temp)
-      etv.value = ''
+    if (event.target.value == words[0] + " ") {
+      setWords(words.slice(1))
+      event.target.value = ''
+      if (words.length == 1){
+        setWpm(calculateWpm(entriesCounter, (timerValue - counter)))
+        setCounter(0)
+      }
     }
 
-    detectError(etv.value, words[0], document.getElementById(0))
-
+    setEntriesCounter(entriesCounter + 1)
+    detectError(event.target.value, words[0], document.getElementById(0))
   }
 
   useEffect(() => {
@@ -47,9 +56,11 @@ function Typetest() {
 
   return (
     <div>
+      
+      <p className='Wpm'> WPM: {wpm} </p>
       <div className="Wordlist">
         <div className='Timer'>
-          {counter ? "00:"+counter : "00:00"}
+          {counter ? "00:" + counter : "00:00"}
         </div>
         <p>
           {words &&
