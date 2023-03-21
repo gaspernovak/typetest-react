5fd97b50d165d555bc16e85d83967f99e844c30b
Gašper Novak <gaspernovak@proton.me>
2023-03-21 20:03:14 +0100

    Fix timer not stopping bug
    Add words from database

--- src/components/Typetest.jsx
+++ src/components/Typetest.jsx
@@ -16,26 +16,46 @@ function calculateWpm(allEntries, timeSeconds) {
 }
 
 function Typetest() {
-  const timerValue = 30 
+  const timerValue = 15
   const [words, setWords] = useState([]);
-  const [counter, setCounter] = useState(0);
-
+  const [counter, setCounter] = useState(-1);
   const [wpm, setWpm] = useState(0);
   const [entriesCounter, setEntriesCounter] = useState(0);
+  const [inputDisabled, setInputDisabled] = useState(false);
 
+  function handlePostLeaderboard() {
+    const record = {
+      "username": "foobar" + Math.floor(Math.random() * 100),
+      "wpm": wpm
+    }
+
+    axios.post('http://localhost:8090/api/collections/leaderboard/records', record)
+      .then(response => console.log(response))
+      .catch((err) => {
+        console.error(err);
+      })
+      .finally(() => {
+        window.location.reload(true)
+      })
+  }
 
   function handleChange(event) {
-    if (counter == 0) {
+    if (counter == -1) {
       setCounter(timerValue)
     }
 
     if (event.target.value == words[0] + " ") {
       setWords(words.slice(1))
       event.target.value = ''
-      if (words.length == 1){
+      if (words.length == 1) {
         setWpm(calculateWpm(entriesCounter, (timerValue - counter)))
         setCounter(0)
       }
+    }
+
+    if(counter == 0){
+      setWpm(calculateWpm(entriesCounter, (timerValue - counter)))
+      setInputDisabled(true)
     }
 
     setEntriesCounter(entriesCounter + 1)
@@ -43,33 +63,41 @@ function Typetest() {
   }
 
   useEffect(() => {
-    axios.get("http://localhost:3001/wordlist").then((data) => {
-      setWords(data?.data);
+    axios.get("http://localhost:8090/api/collections/words/records?perPage=150").then((data) => {
+      let words = []
+      data.data.items.forEach(item => 
+        words.push(item.word)  
+      )
+      setWords(words);
     });
   }, []);
 
   useEffect(() => {
-    const timer =
-      counter > 0 && setInterval(() => setCounter(counter - 1), 1000);
-    return () => clearInterval(timer);
+    const timer = (counter > 0) && setInterval(() => 
+      setCounter(counter - 1), 1000);
+      return () => (
+        clearInterval(timer)
+      )
+      
+
   }, [counter]);
 
   return (
     <div>
-      
       <p className='Wpm'> WPM: {wpm} </p>
+      <button onClick={handlePostLeaderboard}> submit </button>
       <div className="Wordlist">
         <div className='Timer'>
           {counter ? "00:" + counter : "00:00"}
         </div>
         <p>
           {words &&
-            words.map((word, key) =>
+            words.slice(0, 20).map((word, key) =>
               <span className="Word" id={key} key={key}>{word} </span>
             )
           }
         </p>
-        <input autoFocus className="w-100 Word-input" onChange={handleChange} />
+        <input autoFocus className="w-100 Word-input" onChange={handleChange} disabled={inputDisabled} />
       </div>
     </div>
   );
